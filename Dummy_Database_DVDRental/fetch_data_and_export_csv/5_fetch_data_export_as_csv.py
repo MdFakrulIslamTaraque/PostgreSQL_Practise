d374bea1249a2a1c71d0046f514f222924674772
Md Fakrul Islam Taraque <63301304+MdFakrulIslamTaraque@users.noreply.github.com>
2024-04-04 12:38:30 +0600

    segregated the code structure using class inheritance structure and env json

--- Dummy_Database_DVDRental/fetch_data_and_export_csv/5_fetch_data_export_as_csv.py
+++ Dummy_Database_DVDRental/fetch_data_and_export_csv/5_fetch_data_export_as_csv.py
@@ -1,5 +1,44 @@
-import fetch_data_and_export_csv.configuration.connect_db as config
+# import configuration.connect_db
+import configuration.connect_db as configuration
+import pandas as pd
+from psycopg2 import Error
 
-# Create an instance of the Connection class
-worker1 = config.fetcher_and_exporter()
-worker1.view_data_description(worker1.execute('actor'))
+# class to fetch data and export to csv inherits from the connect_db class
+class fetcher_and_exporter(configuration.Credential):
+        
+        # inherit the __init__ method from the connect_db class
+        def __init__(self):
+                super().__init__()
+
+         # view the data fetched and description
+        def view_data_description(self, cursor):
+                print(f"Data fetched: {cursor.rowcount} rows")
+                print(f"Description: {cursor.description}")
+        
+        def fetch_export(self, table_name):
+                try:
+                    cursor = self.connection.cursor()
+                    try:
+                            cursor.execute(f"SELECT * FROM {table_name}")
+                            self.view_data_description(cursor)
+                            data = cursor.fetchall()
+                            columns = [desc[0] for desc in cursor.description]
+                            df = pd.DataFrame(data, columns=columns)
+                            df.to_csv(f'{table_name}.csv', index=False)
+                            print(f'{table_name}.csv has been created')
+                            self.close_connection()
+                            self.close_cursor(cursor)
+                    except Error as e:
+                            print(f"Error: {e}")
+                except Error as e:
+                        print(f"Error: {e}")
+                finally:
+                        self.close_cursor(cursor)
+                        self.close_connection()
+
+# create an instance of the fetcher_and_exporter class
+worker1 = fetcher_and_exporter()
+worker1.fetch_export('actor')
+
+worker2 = fetcher_and_exporter()
+worker2.fetch_export('country')
--- Dummy_Database_DVDRental/fetch_data_and_export_csv/configuration/connect_db.py
+++ Dummy_Database_DVDRental/fetch_data_and_export_csv/configuration/connect_db.py
@@ -1,83 +1,38 @@
-import env_var
 import psycopg2
-import pandas as pd
 from psycopg2 import Error
+import os
+import json
+
+# load the environment variables from config.json file (host, port, database, user, and password)
+config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.json')
+
+# check if the file exists and after that load the environment variables
+with open(config_file, 'r') as file:
+        env_var = json.load(file)
 
 
 # create a class to connect to the database with all other methods
-
-class fetcher_and_exporter:
+class Credential:
         def __init__(self):
-                self.connection = psycopg2.connect(
-                        host= env_var.host,
-                        port=env_var.port,
-                        database=env_var.database,
-                        user=env_var.user,
-                        password=env_var.password
-                )
-
+                try:
+                        # Connect to the PostgreSQL database
+                        self.connection = psycopg2.connect(
+                                host=env_var['host'],
+                                port=env_var['port'],
+                                database=env_var['database'],
+                                user=env_var['user'],
+                                password=env_var['password']
+                        )
+                        print("Connected to the database successfully")
+                except (Exception, Error) as error:
+                        print("Error while connecting to PostgreSQL", error)
         
         #close the connection
         def close_connection(self):
                 self.connection.close()
+                print("Connection closed successfully")
         
         #close the cursor
         def close_cursor(self, cursor):
                 cursor.close()
-
-        #export the data as csv
-        def export_csv(self, table_name, cursor, rows):
-                 # Convert the result set to a Pandas DataFrame
-                df = pd.DataFrame(rows, columns=[col.name for col in cursor.description])
-
-                # df.to_csv('actor.csv', index=False)
-                df.to_csv(f'{table_name}.csv', index=False)
-                print("Data exported as CSV successfully")
-
-        #execute the query
-        def execute_query(self, table_name):
-                return f"SELECT * FROM {table_name}"
-        
-        # view the data fetched and description
-        def view_data_description(self, cursor):
-                print(f"Data fetched: {cursor.rowcount} rows")
-                print(f"Description: {cursor.description}")
-
-        # execute the query and export the data as csv
-        def execute(self, table_name):
-                # Execute your SQL query here
-                sql_query = self.execute_query(table_name)
-                
-                try:
-                        # Connect to the PostgreSQL database
-                        connection = self.connection
-
-                        # Create a new cursor
-                        cursor = connection.cursor()
-
-                        # Execute the SQL query
-                        cursor.execute(sql_query)
-
-                        self.view_data_description(cursor)
-
-                        # Fetch all rows from the result set
-                        rows = cursor.fetchall()
-
-                        self.export_csv(table_name, cursor, rows)
-                        
-                        # Close the cursor and connection
-                        self.close_cursor(cursor)
-
-                        # close the connection
-                        self.close_connection()
-                
-                except (Exception, Error) as error:
-                        print("Error while fetching data from PostgreSQL:", error)
-                finally:
-                        # Close the connection even if an error occurs
-                        if connection:
-                                self.close_connection()
-
-# Create an instance of the Connection class
-worker1 = fetcher_and_exporter()
-worker1.execute('actor')
+                print("Cursor closed successfully")
