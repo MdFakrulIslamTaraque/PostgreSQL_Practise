e846b09ba0f707c2b033bbf99d41ba029fe0d915
Fakrul.Vivasoft <fakrul.islam@vivacomsolutions.com>
2024-04-18 17:00:17 +0600

    conditional insert into table and declare variable

---
+++ Dummy_Database_DVDRental/practice_query/phase_03/conditional_insert_into_table.sql
@@ -0,0 +1,28 @@
+DO $$
+DECLARE
+    current_ts TIMESTAMP :=  CURRENT_TIMESTAMP; -- '2014-05-26 14:50:58.951';
+BEGIN
+    WITH cte AS (
+        SELECT MAX(last_update) AS max_date FROM film
+    )
+    INSERT INTO film (
+        title, description, release_year, language_id, rental_duration, 
+        rental_rate, length, replacement_cost, rating, last_update, 
+        special_features, fulltext
+    )
+    SELECT 
+        'Zorro Ark'
+        ,'A Intrepid Panorama of a Mad Scientist And a Boy who must Redeem a Boy in A Monastery' 
+        ,2006
+        ,1 
+        ,3
+        ,4.99 
+        ,50
+        ,18.99
+        ,'NC-17'
+        ,current_ts
+        ,'{Trailers,Commentaries,"Behind the Scenes"}' 
+        ,'''ark'':2 ''boy'':12,17 ''intrepid'':4 ''mad'':8 ''monasteri'':20 ''must'':14 ''panorama'':5 ''redeem'':15 ''scientist'':9 ''zorro'':1'
+    WHERE 
+        current_ts > (SELECT max_date FROM cte);
+END $$;
