8b982378b404e7592c88e126282f71ffa709282f
Fakrul.Vivasoft <63301304+MdFakrulIslamTaraque@users.noreply.github.com>
2024-09-26 11:02:20 +0600

    learned the ntile() window function to correct the misinterpreted problem.

--- random_learning/concat_group/employee_group.sql
+++ random_learning/concat_group/employee_group.sql
@@ -1,4 +1,8 @@
---I have 1 table, `employee_group` table. Find out a table group of each consecutive 2 employees in a same group and show each gorup's 2 member in same row with `{id1}{name1},{id2}{name2}` 
+-- I have 1 table, `employee_group` table. The output table will be of 4 rows(4 group) max, where each row will be of equal members, if not possible, 
+-- then, uneven members will be distibuted to the groups from group 1 equally
+-- each consecutive 2 employees in a same group and show each gorup's 2 member in same 
+-- e.g, if there are 10 members, then in 4 consecutive group, there will be 3,3,2,2 members
+-- row with `{id1}{name1},{id2}{name2},{id3}{name3}.....` 
 --
 --Given sampe input table:
 --+-----+-----------+
@@ -16,16 +20,14 @@
 --+-----+-----------+
 	
 -- sample output:
---+---------------------+
---|		output			|
---+---------------------+
---|		1emp1,2emp2		|
---|		3emp3,4emp4		|
---|		5emp5,6emp6		|
---|		7emp7,8emp8		|
---|		9emp9,null		|
---+---------------------+
-
+--+-------------------------+
+--|		output				|
+--+-------------------------+
+--|		1emp1,2emp2,3emp3	|
+--|		4emp4,5emp5			|
+--|		6emp6,7emp7			|
+--|		8emp8,9emp9			|
+--+-------------------------+
 --create table public.employee_group(
 --	id int
 --	, name varchar(20)
@@ -49,20 +51,20 @@ select * from public.employee_group eg;
 --;
 
 
-with next1 as(
-	select
-		*
-		, lead(id, 1) over() as next_id
-		, lead(name, 1) over() as next_name
-		, rank() over(order by id) rnk
-	from
-		employee_group eg
+with cte as (
+select
+	concat(cast(id as text),',',"name") as emp
+	, ntile(4) over(order by id) as grp
+from
+	employee_group
 )
 select
-	rnk
-	, concat(id, name, ',', next_id, next_name) as output
+	grp
+	, string_agg(emp,',') as emp
 from
-	next1
-where 
-	mod(rnk,2) = 1
+	cte
+group by 
+	1
+order by 
+	1
 ;
